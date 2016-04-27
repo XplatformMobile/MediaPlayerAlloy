ac081aa432e503c661e1e1b40c7a413df9520870
Vitor Augusto Cotta Xavier <msoe@msoemac08siMac.nebula.msoe.edu>
2016-04-27 14:49:23 -0500

    Wishlist and some code work.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,3 +3,10 @@ function doClick(e) {
 }
 
 $.index.open();
+var items = [
+	{properties: {title: "Music 1"}},
+	{properties: {title: "Music 2"}},
+	{properties: {title: "Music 3"}}
+];
+$.songsList.sections[0].setItems(items);
+
